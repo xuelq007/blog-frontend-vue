b11c75eb772c83ab3560395da2e4d2684c7cd5fb
xuelq007 <xueliqiang1988@sina.com>
2018-12-05 15:02:47 +0800

    改变回到顶部样式

--- src/components/top/Top.vue
+++ src/components/top/Top.vue
@@ -1,6 +1,6 @@
 <template>
     <transition name="fade">
-        <i v-show="visible" class="icon iconfont icon-huidaodingbuicon goTop" title="回到顶部"></i>
+        <i v-show="visible" class="icon iconfont icon-huidaodingbu1 goTop" title="回到顶部"></i>
     </transition>
 </template>
 
